99d8dfc7c243cdb6106bbb7c589e5ae9884aac5c
SaintPeter <rex.schrader@gmail.com>
2016-06-05 10:21:52 -0700

    Update to Font Awesome 4.6

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,7 +18,7 @@
 	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 	<title>Campoliondo High School Class of 1966</title>
 	<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
 	{{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.5/flatly/bootstrap.min.css" rel="stylesheet"> --}}
 	<link href="/css/main.css" rel="stylesheet">
     <link href="/css/jquery-confirm.css" rel="stylesheet">
