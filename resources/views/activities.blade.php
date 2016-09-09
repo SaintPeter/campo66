6f91318333fa502d03eae4cfa2e23b0159a76021
SaintPeter <rex.schrader@gmail.com>
2016-09-09 10:15:18 -0700

    Update Activities

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -2,30 +2,48 @@
 
 @section('content')
 <div class="col-md-2"></div>
-<div class="col-md-8">
+<div class="col-md-8 activities">
     <h1>Activities</h1>
     <div class="panel panel-default">
         <div class="panel-heading"><h2 class="panel-title">Friday, October 14, 2016</h2></div>
         <div class="panel-body">
-            <strong>Afternoon</strong> &mdash; Tour of Campolindo<br>
-            <strong>Evening </strong> &mdash; Away football game or a casual gathering at the Embassy Suites Lounge 7:00pm
+            <strong>11:45AM &mdash; Panel Discussion/Presentation</strong><br>
+            <p>Meet with the current students at Campolindo in the small gym (between back parking lot  & original gym).  Visitor nametags will be given in the gym. Co-chairs are Dave Hobbs and Roger Ganse.  Do not arrive before 11:30 please; park in the neighborhood, beware "no parking" signs.  Lunch is not provided.</p>
+
+            <strong>3:15PM &mdash; Campolindo Guided Tour</strong><br>
+            <p>Meet in the small gym (between back parking lot & original gym).  Visitor nametags will be given in the gym.</p>
+            <strong>7:00pm &mdash; Varsity Football Game at Miramonte</strong> (Optional)<br><br>
+            <strong>7:00pm &mdash; Connect Early</strong><br>
+            <p>Informal get together at: <br>
+            Embassy Suites<br>
+            1345 Treat Blvd.<br>
+            Walnut Creek<br>
+            925-934-2500 <br>
+            (closest hotel to the Saturday night Back Forty Texas BBQ event).  Meet in the Embassy Lounge/Bar for visiting, no host drinks & hors d&apos;oeuvres.  Embassy Grill is available for a no-host meal.</p>
          </div>
     </div>
 
     <div class="panel panel-default">
         <div class="panel-heading"><h2 class="panel-title">Saturday, October 15, 2016</h2></div>
         <div class="panel-body">
-            <strong>Morning</strong> &mdash; Fun gathering and games at the Moraga Commons park<br>
-            <strong>Evening</strong> &mdash; Texas Back 40 in Pleasant Hill<br>
-            5:00-9:00 in the upper room<br>
-            Casual event with buffet dinner and no host bar<br><br>
+            <strong>10:00-12:00 &mdash; Fun Morning</strong><br>
+            <p>Casual gathering at Moraga Commons Park (at the corner of St. Mary&apos;s and Moraga Roads.  Overflow parking in the dirt off Moraga Road near Alta Mesa Dr.) - bocce ball, socializing, games, silliness.  Kids & grandchildren welcome.</p>
+            <strong>5:00-9:00 &mdash; Reunion Celebration at Back Forty Texas BBQ</strong><br>
+            <p>100 Coggins Dr.<br>
+            Pleasant Hill<br>
+            Delicious, lip smackin&apos; buffet dinner, no host bar, not-to-be-missed surprise awards, cool slide show presentation, group photo.<br>
+            A relaxed evening renewing friendships and catching up on yesteryear.<br>
+	        <strong>Cost:</strong>  $66 per classmate; $33 for guest/spouse.<br>
+	        Come single or with a guest.<br><br>
 
-            Cost will be minimal (Thanks to generous donors.
-            If you would like to donate, please <a href="{{ route('contact') }}">Contact Us</a>)<br>
+	        <strong>Reservation and payment due by September 30!</strong><br><br>
+(Parking is minimal; there is valet parking; street parking; and lot parking across the street by Office Depot or Bassett Furniture, NOT by Best Buy please; Pleasant Hill BART is free after 3:00 and is walkable.)
+</p>
 
-            <h4>Hotel Suggestions</h4>Embassy Suites (Walnut Creek at the Pleasant Hill BART)<br>
+            <h4>Hotel Suggestions</h4>
+            <p>Embassy Suites (Walnut Creek at the Pleasant Hill BART)<br>
             Ext. Stay America (Pleasant Hill)<br>
-            Holiday Inn (Walnut Creek)
+            Holiday Inn (Walnut Creek)</p>
          </div>
     </div>
 
