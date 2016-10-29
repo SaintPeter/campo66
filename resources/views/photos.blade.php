71d3ccb8fc3ee2626d61f2f361856058207d6af5
SaintPeter <rex.schrader@gmail.com>
2016-10-29 13:07:59 -0700

    Add more instructions

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -3,20 +3,21 @@
 @section('content')
 <div class="col-md-2"></div>
 <div class="col-md-8 activities">
-    <h1>Reunion Photos</h1>
-    <h2>10/14 - Campus Reunion</h2>
+    <h1>Reunion Photo Albums</h1>
+    Mouse Over each picture and click Next/Back buttons to see more!
+    <h2>10/14 - Campus Reunion Album</h2>
     <small>Photos by Bruce Van Voorhis</small>
     <a data-flickr-embed="true"  href="https://www.flickr.com/photos/146055081@N04/albums/72157672237052924" title="2016-10-14 Campolindo Reunion Campus Visit Bruce Van Voorhis&#x27; Pictures"><img src="https://c5.staticflickr.com/6/5455/30511434812_cdbabde9b4_c.jpg" width="800" height="451" alt="2016-10-14 Campolindo Reunion Campus Visit Bruce Van Voorhis&#x27; Pictures"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 
-    <h2>10/15 Picnic</h2>
+    <h2>10/15 Picnic Album</h2>
     <small>Photos by Bruce Van Voorhis</small>
     <a data-flickr-embed="true"  href="https://www.flickr.com/photos/146055081@N04/albums/72157674655205560" title="2016-10-15 Moraga Commons Picnic Bruce Van Voorhis&#x27; Pictures"><img src="https://c4.staticflickr.com/6/5448/30540231811_e942f16ef0_c.jpg" width="800" height="451" alt="2016-10-15 Moraga Commons Picnic Bruce Van Voorhis&#x27; Pictures"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 
-    <h2>10/15 Dinner</h2>
+    <h2>10/15 Dinner Album</h2>
     <small>Photos by Bruce Van Voorhis and Doug Landin</small>
     <a data-flickr-embed="true"  href="https://www.flickr.com/photos/146055081@N04/albums/72157674431238441" title="2016-10-15 Dinner Bruce Van Voorhis&#x27; Pictures (many by Doug Landin)"><img src="https://c7.staticflickr.com/6/5543/30511908502_25b97197b9_c.jpg" width="800" height="451" alt="2016-10-15 Dinner Bruce Van Voorhis&#x27; Pictures (many by Doug Landin)"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 
-    <h2>10/15 Dinner</h2>
+    <h2>10/15 Dinner Album</h2>
     <small>Photos by Dennis Scott</small>
     <a data-flickr-embed="true"  href="https://www.flickr.com/photos/146055081@N04/albums/72157672244703893" title="2016-10-15 Reunion Dinner Dennis Scott&#x27;s Pictures"><img src="https://c7.staticflickr.com/6/5587/30511181982_6d4566f3a7_c.jpg" width="800" height="534" alt="2016-10-15 Reunion Dinner Dennis Scott&#x27;s Pictures"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 
